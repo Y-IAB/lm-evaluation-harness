d1bbf404a6bf5756e0ccf8e2eaaa402ab77384e9
suhyun.kang <suhyun.kang@yanolja.group>
2024-03-07 16:13:36 +0900

    Add Azure

--- lm_eval/tasks/wmt2016/metrics.py
+++ lm_eval/tasks/wmt2016/metrics.py
@@ -2,9 +2,11 @@ import json
 import os
 
 import evaluate
-from uptrain import EvalLLM, Evals
+from uptrain import EvalLLM, Evals, CritiqueTone, Settings
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+AZURE_API_KEY = os.environ.get("AZURE_API_KEY")
+AZURE_API_VERSION = os.environ.get("AZURE_API_VERSION")
+AZURE_ENDPOINT = os.environ.get("AZURE_ENDPOINT")
 
 
 def bleu(predictions, references):
@@ -27,6 +29,15 @@ def bleurt(predictions, references):
 def llm_eval(predictions, references):
     data = [{"response": predictions[0]}]
 
-    eval_llm = EvalLLM(openai_api_key=OPENAI_API_KEY)
+    if not AZURE_API_KEY or not AZURE_API_VERSION or not AZURE_ENDPOINT:
+        raise ValueError(
+            "Please set the environment variables AZURE_API_KEY, AZURE_API_VERSION and AZURE_ENDPOINT"
+        )
+
+    eval_llm = EvalLLM(
+        Settings(model='azure/gpt-4-turbo',
+                 azure_api_key=AZURE_API_KEY,
+                 api_version=AZURE_API_VERSION,
+                 azure_api_base=AZURE_ENDPOINT))
     response = eval_llm.evaluate(data, checks=[Evals.CRITIQUE_LANGUAGE])
-    return response[0]["score_fluency"]
+    return response[0]["score_critique_language"]

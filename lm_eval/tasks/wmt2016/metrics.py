3ce1372c13db9b0e73f9d1509141fb712bb1c89a
suhyun.kang <suhyun.kang@yanolja.group>
2024-03-07 14:56:02 +0900

    Add a new task

--- lm_eval/tasks/wmt2016/metrics.py
+++ lm_eval/tasks/wmt2016/metrics.py
@@ -1,4 +1,10 @@
+import json
+import os
+
 import evaluate
+from uptrain import EvalLLM, Evals
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 
 def bleu(predictions, references):
@@ -8,4 +14,19 @@ def bleu(predictions, references):
 def agg_bleu(items):
     bleu_fn = evaluate.load("bleu")
     predictions, references = zip(*items)
-    return bleu_fn.compute(predictions=predictions, references=references)["bleu"]
+    return bleu_fn.compute(predictions=predictions,
+                           references=references)["bleu"]
+
+
+def bleurt(predictions, references):
+    bleurt_fn = evaluate.load("bleurt")
+    return bleurt_fn.compute(predictions=predictions,
+                             references=references)["scores"][0]
+
+
+def llm_eval(predictions, references):
+    data = [{"response": predictions[0]}]
+
+    eval_llm = EvalLLM(openai_api_key=OPENAI_API_KEY)
+    response = eval_llm.evaluate(data, checks=[Evals.CRITIQUE_LANGUAGE])
+    return response[0]["score_fluency"]

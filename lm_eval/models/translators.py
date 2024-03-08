530952c84aa4aa0cc9ca2c82759e9a5e1a8da260
suhyun.kang <suhyun.kang@yanolja.group>
2024-03-08 14:31:53 +0900

    Replace translator

--- lm_eval/models/translators.py
+++ lm_eval/models/translators.py
@@ -1,39 +1,33 @@
 import json
-import logging
-import time
-from typing import List, Literal, Optional, Tuple, Union
+from typing import List, Optional, Tuple, Union
 
-import deepl
-import httpx
-import openai
 import requests
-from openai import AzureOpenAI
 from tqdm import tqdm
 
 from lm_eval.api.instance import Instance
 from lm_eval.api.model import LM
 from lm_eval.api.registry import register_model
 
-logging.basicConfig()
-logging.getLogger('deepl').setLevel(logging.ERROR)
 
-
-@register_model("deepl")
-class DeepLTranslator(LM):
+@register_model("translator")
+class Translator(LM):
 
     def __init__(self,
                  api_key: str,
-                 source_lang: str,
+                 endpoint: str,
                  target_lang: str,
+                 model: str,
                  batch_size: Optional[Union[int, str]] = 1):
         super().__init__()
 
-        self.api_key = api_key
+        self.endpoint = endpoint
+        self.headers = {
+            "Authorization": api_key,
+            'Content-Type': 'application/json'
+        }
 
-        self.source_lang = self._convert_lang_to_code(source_lang)
         self.target_lang = self._convert_lang_to_code(target_lang)
-
-        self.translator = deepl.Translator(self.api_key)
+        self.model = model
 
         # don't know how to use it yet
         self.batch_size = batch_size
@@ -45,32 +39,31 @@ class DeepLTranslator(LM):
             # The first argument is the formatted doc_to_text text.
             source_text = request.args[0]
 
-            try:
-                result = self.translator.translate_text(
-                    source_text,
-                    target_lang=self.target_lang,
-                    # Do we need these options?
-                    # preserve_formatting=False,
-                    # context=
-                    # tag_handling='xml',
-                )
-                results.append(result.text)
+            data = {
+                "text": [source_text],
+                "target_lang": self.target_lang,
+                "model": self.model
+            }
 
-            except deepl.exceptions.DeepLException as e:
-                print(f"An error occurred while translating: {e}")
-                raise e
+            response = requests.post(self.endpoint,
+                                     headers=self.headers,
+                                     json=data)
+
+            if response.status_code != 200:
+                print(f"An error occurred while translating: {response.text}")
+                raise Exception(response.text)
+
+            result = response.json()
+            results.append(result['translations'][0]['text'])
 
         return results
 
     def _convert_lang_to_code(self, lang: str) -> str:
-        # Convert the language name to the language code
-        # https://www.deepl.com/docs-api/translate-text/?utm_source=github&utm_medium=github-python-readme
         lang_code = {
-            'english': 'EN-US',
-            'korean': 'KO',
-            'japanese': 'JA',
-            'chinese': 'ZH',
-            'romanian': 'RO',
+            'en': 'EN-US',
+            'ko': 'KO',
+            'ja': 'JA',
+            'ch': 'ZH-CN',
         }
         return lang_code[lang.lower()]
 

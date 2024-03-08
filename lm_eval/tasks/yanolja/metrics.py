cf754c35f8cb40a4727df1db93a3f5878496ad6e
suhyun.kang <suhyun.kang@yanolja.group>
2024-03-08 16:06:12 +0900

    Update doc_to_target

--- lm_eval/tasks/yanolja/metrics.py
+++ lm_eval/tasks/yanolja/metrics.py
@@ -2,11 +2,36 @@ import json
 import os
 
 import evaluate
+from comet import download_model, load_from_checkpoint
 from uptrain import CritiqueTone, EvalLLM, Evals, Settings
 
 AZURE_API_KEY = os.environ.get("AZURE_API_KEY")
 AZURE_API_VERSION = os.environ.get("AZURE_API_VERSION")
 AZURE_ENDPOINT = os.environ.get("AZURE_ENDPOINT")
+
+
+def bleu(predictions, references):
+    prediction, reference = predictions[0], json.loads(
+        references[0])["reference"]
+
+    bleu_fn = evaluate.load("bleu")
+    return bleu_fn.compute(predictions=[prediction],
+                           references=[reference])["bleu"]
+
+
+def xcomet(predictions, references):
+    prediction, source = predictions[0], json.loads(references[0])["source"]
+
+    data = [{"src": source, "mt": prediction}]
+
+    model_path = download_model("Unbabel/wmt22-cometkiwi-da")
+    model = load_from_checkpoint(model_path)
+
+    model_output = model.predict(data, batch_size=8, gpus=0)
+
+    # Example output:
+    # Prediction([('scores', [0.8676194548606873]), ('system_score', 0.8676194548606873)])
+    return model_output[0][0]
 
 
 def llm_eval(predictions, references):

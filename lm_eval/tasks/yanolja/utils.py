a0df144d416c092f7b3f7c5d9cb92f7253ff3df7
seungtaek.choi <seungtaek.choi@yanolja.com>
2024-05-24 10:46:28 +0900

    Rename get_answer_idx to get_target

--- lm_eval/tasks/yanolja/utils.py
+++ lm_eval/tasks/yanolja/utils.py
@@ -6,7 +6,7 @@ def translation_mc_get_prompt_en_ko(doc: dict) -> str:
     return f"""You are a helpful AI assistant that is translating English to Korean. The English text is given below. Please start the translated text in Korean.
     English: {doc["segments"]["sourceText"]}"""
 
-def translation_mc_get_answer_idx(doc: dict) -> str:
+def translation_mc_get_target(doc: dict) -> str:
     return f"""{doc["segments"]["targetText"]}"""
 
 def translation_mc_get_choices(doc: dict) -> str:
